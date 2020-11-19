3437326cb1832caeca827b85a283de82a1d498aa
RedBirdOBX <shane.fowlkes.70@gmail.com>
2020-11-19 16:24:25 -0500

    clean up

--- react-playground/src/Apps/ObjectMatchGame/ObjectMatchGameContainer.js
+++ react-playground/src/Apps/ObjectMatchGame/ObjectMatchGameContainer.js
@@ -1,6 +1,16 @@
 import React, {useState} from 'react';
 import ObjectMatchGameComponent from './Components/ObjectMatchGameComponent';
 
+// ===============================================================================
+// TODO:
+//      add prop validation
+//      // adding new property of type 'propTypes'.
+// Answer.propTypes =
+// {
+//     Answer: PropTypes.string.isRequired,
+//     Author: PropTypes.object.isRequired
+// }
+// ===============================================================================
 
 const ObjectMatchGameContainer = (props) =>
 {
